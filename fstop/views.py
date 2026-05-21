ec45b0038d77c759cf5c7e8866db53a5a0c2b88e
Deanna Thompson <59544150+itsdeannat@users.noreply.github.com>
2026-05-20 21:47:09 -0400

    feat: add views

--- fstop/views.py
+++ fstop/views.py
@@ -1,3 +1,49 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework import viewsets
+from rest_framework.viewsets import ModelViewSet
+from .models import Client, Project, Booking, Gallery
+from .serializers import ClientSerializer, ProjectSerializer, BookingSerializer, GallerySerializer
 
 # Create your views here.
+
+
+class ClientViewSet(viewsets.ModelViewSet):
+    queryset = Client.objects.all()
+    serializer_class = ClientSerializer
+
+
+class ProjectViewSet(viewsets.ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+
+    def get_queryset(self):
+        queryset = Project.objects.all()
+        client_id = self.request.query_params.get('client_id')
+        if client_id:
+            queryset = queryset.filter(client_id=client_id)
+        return queryset
+
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+
+    def get_queryset(self):
+        queryset = Booking.objects.all()
+        project_id = self.request.query_params.get('project_id')
+        if project_id:
+            queryset = queryset.filter(project_id=project_id)
+        return queryset
+
+class GalleryViewSet(viewsets.ModelViewSet):
+    queryset = Gallery.objects.all()
+    serializer_class = GallerySerializer
+
+    def get_queryset(self):
+        queryset = Gallery.objects.all()
+        project_id = self.request.query_params.get('project_id')
+        if project_id:
+            queryset = queryset.filter(project_id=project_id)
+        return queryset
